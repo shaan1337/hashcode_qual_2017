2fec621dc418dd56e43b0e448d30728b47b641f7
shaan1337 <sniper111@gmail.com>
2019-02-27 21:45:56 +0400

    New solution using priority queue and greedily adding videos that cause the largest latency gains

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using C5;
 
 namespace solution
 {
@@ -9,17 +10,20 @@ namespace solution
         static List<int> S = new List<int>();
         static int [,]LAT_E_TO_C = new int [1005,1005];
         static Dictionary<int, List<int> > EDGES_E_TO_C = new Dictionary<int, List<int> >();
+        static Dictionary<int, List<int> > EDGES_E_TO_V = new Dictionary<int, List<int> >();
         static int []LAT_E_TO_D = new int [1005];
         static int [,]REQ = new int[1000005,3];
         static bool [,]MAY_NEED_VIDEO = new bool[1005,10005];
+        static int [,]ENDPOINT_TO_VIDEO_REQUESTS = new int[1005,10005];
         static long MAXSCORE;
         static long TOTALREQUESTS = 0L;
-
-        static List<HashSet<int>> Assignment = new List<HashSet<int>>();
-        static List<HashSet<int>> BestAssignment = new List<HashSet<int>>();
+        static List<C5.HashSet<int>> Assignment = new List<C5.HashSet<int>>();
+        static List<C5.HashSet<int>> BestAssignment = new List<C5.HashSet<int>>();
         static long []SpaceUsed= new long[1005];
         static long BestScore = 0L;
         static Random Random = new Random();
+        static C5.IPriorityQueue<Tuple<long,int,int>> sortedLatencyGains = new IntervalHeap<Tuple<long,int,int>>();
+        static C5.IPriorityQueueHandle<Tuple<long, int, int>>[,] latencyGainHandles = new C5.IPriorityQueueHandle<Tuple<long, int, int>>[1005,10005];
 
         static void Main(string[] args)
         {
@@ -32,7 +36,10 @@ namespace solution
             Console.Out.WriteLine("Max theoretical score: {0}", 1000.0*MAXSCORE/TOTALREQUESTS);
             //PrintAssignment(Assignment);
 
-            while(true){
+            var iter = 0;
+            while(iter<1){
+                iter++;
+
                 for(int c=0;c<C;c++){
                     Assignment[c].Clear();
                     SpaceUsed[c] = 0;
@@ -47,7 +54,7 @@ namespace solution
             }
         }
 
-        private static void PrintAssignment(List<HashSet<int>> assignment)
+        private static void PrintAssignment(List<C5.HashSet<int>> assignment)
         {
             for(int c=0;c<assignment.Count;c++){
                 Console.Out.Write(c);
@@ -83,55 +90,73 @@ namespace solution
         }
         private static long Solve()
         {
-            List<Tuple<long, int> > bestLatencySavingsEndpoints = new List<Tuple<long, int>>();
-            for(var e=0;e<E;e++){
-                var maxLatency = 0L;
-                foreach(var c in EDGES_E_TO_C[e]){
-                    maxLatency = Math.Max(LAT_E_TO_C[e,c], maxLatency);
+            Console.Out.Write("Initial generation of latency gains... ");
+
+            long [,]latencyGains = new long[C,V];
+
+            for(var v=0;v<V;v++){
+                if(S[v] > X) continue;
+                for(var c=0;c<C;c++){
+                    var totalLatencyGain = 0L;
+                    for(var e=0;e<E;e++){
+                        if(LAT_E_TO_C[e,c] == int.MaxValue) continue;
+                        var numRequests = ENDPOINT_TO_VIDEO_REQUESTS[e,v];
+                        if(numRequests == 0) continue;
+                        var latencyGain = (long)(LAT_E_TO_D[e] - LAT_E_TO_C[e,c]) * numRequests;
+                        totalLatencyGain += latencyGain;
+                    }
+                    C5.IPriorityQueueHandle<Tuple<long, int, int>> handle = null;
+                    sortedLatencyGains.Add(ref handle, new Tuple<long, int, int>(totalLatencyGain, c, v));
+                    latencyGainHandles[c,v] = handle;
+                    latencyGains[c,v] = totalLatencyGain;
                 }
-                var minLatencySavings = LAT_E_TO_D[e] - maxLatency;
-                bestLatencySavingsEndpoints.Add(new Tuple<long,int>(minLatencySavings, e));
             }
+            Console.Out.WriteLine("Done!");
 
-            bestLatencySavingsEndpoints.Sort();
-            bestLatencySavingsEndpoints.Reverse();
+            while(!sortedLatencyGains.IsEmpty){
+                var best = sortedLatencyGains.FindMax();
+                sortedLatencyGains.DeleteMax();
+                //Console.Out.WriteLine("--> gain: "+best.Item1+" c: "+best.Item2+" v: "+best.Item3);
+                var gain = best.Item1;
+                var c = best.Item2;
+                var v = best.Item3;
 
-            Dictionary<int, List<Tuple<int, int> > > endpointToVideoRequests = new Dictionary<int, List<Tuple<int, int>>>();
-            for(int e=0;e<E;e++) endpointToVideoRequests[e] = new List<Tuple<int, int>>();
+                if(S[v] + SpaceUsed[c] > X) continue;
+                Assignment[c].Add(v);
+                SpaceUsed[c] += S[v];
 
-            for(int r=0;r<R;r++){
-                int v = REQ[r,0];
-                int e = REQ[r,1];
-                int n = REQ[r,2];
+                for(var cx=0;cx<C;cx++)
+                    latencyGains[cx,v] = 0L;
 
-                endpointToVideoRequests[e].Add(new Tuple<int, int>(n, v));
-            }
+                for(var ex=0;ex<E;ex++){
+                    var bestCache = -1;
+                    var bestLatency = LAT_E_TO_D[ex];
 
-            for(int e=0;e<E;e++){
-                endpointToVideoRequests[e].Sort();
-                endpointToVideoRequests[e].Reverse();
-            }
+                    foreach(var cx in EDGES_E_TO_C[ex]){
+                        if(!Assignment[cx].Contains(v)) continue;
+                        if(LAT_E_TO_C[ex, cx] < bestLatency){
+                            bestCache = cx;
+                            bestLatency = LAT_E_TO_C[ex, cx];
+                        }
+                    }
 
-            var hasMore = false;
-            do{
-            hasMore = false;
-            foreach(var ee in bestLatencySavingsEndpoints){
-                var e = ee.Item2;
-                foreach(var vv in endpointToVideoRequests[e]){
-                    var v = vv.Item2;
-                    var n = vv.Item1;
-                    foreach(var c in EDGES_E_TO_C[e]){
-                        if(!Assignment[c].Contains(v) && SpaceUsed[c] + S[v] <= X){
-                                hasMore = true;
-                                if(Random.Next()%2 == 0){
-                                    Assignment[c].Add(v);
-                                    SpaceUsed[c] += S[v];
-                                }
+                    foreach(var cx in EDGES_E_TO_C[ex]){
+                        if(Assignment[cx].Contains(v)) continue;
+                        if(LAT_E_TO_C[ex, cx] < bestLatency){
+                            latencyGains[cx, v] += (bestLatency - LAT_E_TO_C[ex,cx]) * ENDPOINT_TO_VIDEO_REQUESTS[ex, v];
                         }
                     }
                 }
+
+                for(var cx=0;cx<C;cx++){
+                    var handle = latencyGainHandles[cx, v];
+                    Tuple<long, int, int> item;
+                    if(sortedLatencyGains.Find(handle, out item)){
+                        sortedLatencyGains.Delete(handle);
+                        sortedLatencyGains.Add(ref handle, new Tuple<long, int, int>(latencyGains[cx, v], cx, v));
+                    }
+                }
             }
-            }while(hasMore);
 
             VerifySolution();
             return CalculateScore();
@@ -140,13 +165,14 @@ namespace solution
         private static void Prefill(){
             Console.Out.WriteLine("Prefilling values");
             for(int c=0;c<C;c++){
-                Assignment.Add(new HashSet<int>());
+                Assignment.Add(new C5.HashSet<int>());
             }
 
             for(int r=0;r<R;r++){
                 int v = REQ[r,0];
                 int e = REQ[r,1];
                 int n = REQ[r,2];
+                ENDPOINT_TO_VIDEO_REQUESTS[e,v] = n;
                 TOTALREQUESTS += n;
                 foreach(var c in EDGES_E_TO_C[e]){
                     MAY_NEED_VIDEO[c, v] = true;
